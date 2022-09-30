7d89ff49d624aa7364d2ac177508d3e1a6b07000
AAPPO_LAUNONEN <aappo.launonen@outlook.com>
2022-09-30 09:36:31 +0300

    Eka pusku

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+
+  const [mailit, setMailit] = useState('')
+  const [kilometrit, setKilometrit] = useState('')
+
+  function laske(e) {
+    e.preventDefault()
+    const tulos = mailit * 1.609
+    setKilometrit(tulos)
+  }
+
+    return (
+      <div id="content">
+        <h3>Mailit kilometreiksi</h3>
+        <form onSubmit={laske}>
+          <div>
+            <label>Mailit</label>
+            <input value={mailit} onChange={e => setMailit(e.target.value)}/>
+          </div>
+          <div>
+            <label>Kilometrit</label>
+            <output>{kilometrit.toFixed(2)}</output>
+          </div>
+          <button>Laske</button>
+        </form>
+      </div>
+    )
+}
+
+export default App;
